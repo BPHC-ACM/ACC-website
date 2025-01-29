a11411f1baffff6b54bded3cdf1335908145b4ae
ShreeSarkar22 <shreesarkar2020@gmail.com>
2025-01-30 01:21:14 +0530

    Added the fields of cg and branch

--- app/api/chat-requests/route.js
+++ app/api/chat-requests/route.js
@@ -7,54 +7,72 @@ export async function GET() {
       iconUrl: "/placeholder.svg?height=40&width=40",
       text: "Need Help in Frontend",
       time: "5m ago",
+      cgpa : "7.0",
+      branch : "EEE"
     },
     {
         name: "Ritesh",
         iconUrl: "/placeholder.svg?height=40&width=40",
         text: "ANC ajao",
         time: "1m ago",
+        cgpa : "8.0",
+        branch : "ECE"
     },
     {
         name: "Devansh",
         iconUrl: "/placeholder.svg?height=40&width=40",
         text: "Update on the project",
         time: "10m ago",
+        cgpa : "6.9",
+        branch : "CS"
     },
     {
         name: "Dhairya",
         iconUrl: "/placeholder.svg?height=40&width=40",
         text: "Hey, can we chat about the project?",
         time: "1h ago",
+        cgpa : "7.0",
+        branch : "EEE"
     },
     {
         name: "Aravind",
         iconUrl: "/placeholder.svg?height=40&width=40",
         text: "Missed the meet",
         time: "2h ago",
+        cgpa : "7.0",
+        branch : "EEE"
     },
     {
       name: "Shashwat",
       iconUrl: "/placeholder.svg?height=40&width=40",
       text: "I have a question about the latest update.",
       time: "15m ago",
+      cgpa : "7.0",
+      branch : "EEE"
     },
     {
       name: "Vikashh",
       iconUrl: "/placeholder.svg?height=40&width=40",
       text: "Goodnight",
       time: "30m ago",
+      cgpa : "7.0",
+      branch : "EEE"
     },
     {
       name: "Arinjay",
       iconUrl: "/placeholder.svg?height=40&width=40",
       text: "Meet ajao",
       time: "1m ago",
+      cgpa : "7.0",
+      branch : "EEE"
     },
     {
       name: "Arinjay",
       iconUrl: "/placeholder.svg?height=40&width=40",
       text: "Nvm cancelled",
       time: "1m ago",
+      cgpa : "7.0",
+      branch : "EEE"
     },
   ]
 
