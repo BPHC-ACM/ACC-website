5ea1bc600d7993a5ef913f6abac764e85d10430b
Aravind Sathesh <aravind.sathesh@gmail.com>
2025-06-17 11:20:46 +0530

    fix: enable RLS on all tables and use service key in backend

--- app/utils/supabaseClient.ts
+++ app/utils/supabaseClient.ts
@@ -1,13 +1,22 @@
 import { createClient } from '@supabase/supabase-js';
 
-if (
-	!process.env.NEXT_PUBLIC_SUPABASE_URL ||
-	!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-) {
-	throw new Error('Missing Supabase environment variables');
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+
+const isServer = typeof window === 'undefined';
+
+const supabaseKey = isServer
+	? process.env.SUPABASE_SERVICE_ROLE_KEY
+	: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl) {
+	throw new Error('Missing Supabase URL.');
 }
 
-export const supabase = createClient(
-	process.env.NEXT_PUBLIC_SUPABASE_URL,
-	process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
+if (!supabaseKey) {
+	const errorMessage = isServer
+		? 'Missing Supabase Service Role Key.'
+		: 'Missing Supabase Anon Key.';
+	throw new Error(errorMessage);
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
